1adbd5e6c7767994c21bd26cc600a082f4440caf
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-26 15:20:54 +0530

    Initial Commit

---
+++ CoreDataSwiftUISandbox/CoreDataSwiftUISandboxApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataSwiftUISandboxApp.swift
+//  CoreDataSwiftUISandbox
+//
+//  Created by Atin Agnihotri on 26/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataSwiftUISandboxApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
